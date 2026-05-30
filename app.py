856939d6d997634318dfa15c9d1e5f483e0b8a6a
ywarthonv-source <ywarthonv@gmail.com>
2026-05-30 17:22:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if not st.session_state["autenticado"]:
     # Botones de acceso rapido
     col_inv, col_web = st.columns(2)
     with col_inv:
-        if st.button("Acceso Startup Chile", use_container_width=True, type="primary"):
+        if st.button("Acceso Invitado", use_container_width=True, type="primary"):
             st.session_state["autenticado"]    = True
             st.session_state["usuario_actual"] = "invitado"
             st.rerun()
